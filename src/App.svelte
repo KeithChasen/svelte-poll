e0993c83519f49f8f956f48ba9dc8f39b841bdb3
keithchasen <keithchasen89@gmail.com>
2021-12-09 14:44:51 +0200

    tabs component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,25 @@
 <script>
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
+	import Tabs from "./shared/Tabs.svelte";
+
+	//tabs
+	let tabs = ['Current Polls', 'Add New Poll'];
+	let activeTab = 'Current Polls';
+
+	const tabChange = (e) => {
+		activeTab = e.detail;
+	}
 </script>
 
 <Header />
 <main>
-	<h1>Hello!</h1>
+	<Tabs {activeTab} {tabs} on:tabChange={tabChange} />
+	{#if activeTab === 'Current Polls'}
+		<p>Poll list component</p>
+	{:else if activeTab === 'Add New Poll'}
+		<p>New poll</p>
+	{/if}
 </main>
 <Footer />
 
